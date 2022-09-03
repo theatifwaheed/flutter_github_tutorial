301a88278efa8d8f059907356d3b39066868cab0
TheAtifWaheed <theatifwaheed@gmail.com>
2022-09-04 02:40:13 +0500

    start

---
+++ android/app/src/main/kotlin/com/theatifwaheed/github_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.theatifwaheed.github_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Screens/LoginScreen/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreenHome extends StatefulWidget {
+  const LoginScreenHome({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreenHome> createState() => _LoginScreenHomeState();
+}
+
+class _LoginScreenHomeState extends State<LoginScreenHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
