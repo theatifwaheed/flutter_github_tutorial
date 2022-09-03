c2e279a3b82f85801ecc9fbf14f5c29a3096554b
TheAtifWaheed <theatifwaheed@gmail.com>
2022-09-04 03:21:55 +0500

    loginScreenOnlyTop

--- lib/Screens/LoginScreen/login_screen.dart
+++ lib/Screens/LoginScreen/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreenHome extends StatefulWidget {
   const LoginScreenHome({Key? key}) : super(key: key);
@@ -11,7 +13,32 @@ class _LoginScreenHomeState extends State<LoginScreenHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 34,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30.h,
+              ),
+              SizedBox(
+                height: 176.h,
+                child: SvgPicture.asset(
+                  "assets/graphics/LoginIllustration.svg",
+                ),
+              ),
+              SizedBox(
+                height: 40.h,
+              ),
 
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
